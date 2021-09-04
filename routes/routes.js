92803b1caf91a15e95c1627cb9201e2536b385c0
Mihir More <mihirmore.25@gmail.com>
2021-09-04 19:00:44 +0530

    Second Commit

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,32 @@ const router = express.Router();
 const Product = require("../models/product");
 const Category = require("../models/category");
 
+// Get All The products route
+router.get("/products", async (req, res) => {
+    await Product.find().populate(
+        {
+            path: "categories"
+        }
+    ).exec((err, products) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            res.render("index", {
+                title: "Home Page",
+                products: products
+            });
+        }
+
+    });
+});
+
+// Get the add product route
+router.get("/addProduct", (req, res) => {
+    res.render("add_products", { title: "Add Products" });
+});
+
 // Insert an product into database route
-router.post("/addProduct", (req, res) => {
+router.post("/addProduct", async (req, res) => {
     const product = new Product({
         productName: req.body.productName
     });
@@ -19,79 +43,123 @@ router.post("/addProduct", (req, res) => {
                 type: "success",
                 message: "Product added successfully!"
             }
-            
-            res.redirect("/");
+
+            res.redirect("/products");
         }
-    })
+    });
     
 });
 
-// Insert an Category into database route
-router.post("/addCategory", (req, res) => {
-    const category = new Category({
-        categoryName: req.body.categoryName
-    });
-
-    category.save(err => {
-        if (err) {
-            res.json({
-                message: err.message, type: 'danger'
-            });
-        } else {
-            req.session.message = {
-                type: "success",
-                message: "Category added successfully!"
-            }
-
-            res.redirect("/categories");
-        }
-    });
-});
 
 
+router.get("/products/:id/addCategory", (req, res) => {
+    let id = req.params.id;
 
-
-// Get All The products route
-router.get("/", (req, res) => {
-    Product.find().exec((err, products) => {
+    Product.findById(id).exec((err, product) => {
         if (err) {
             res.json({ message: err.message });
         } else {
-            res.render("index", {
-                title: "Home Page",
+            res.render("add_category", {
+                title: "Add Category",
+                product: product
+            });
+        }
+    });
+    
+});
+
+// Product.findById(id, (err, product) => {
+//     if (err) {
+//         res.redirect("/");
+//     } else {
+//         if (product == null) {
+//             res.redirect("/");
+//         } else {
+//             res.render("add_category", {
+//                 title: "Add Category",
+//                 product: product
+//             });
+//         }
+
+//     }
+
+// });
+
+
+// Route for creating a new Category and updating Product "category" field with it
+router.post("/products/:id/addCategory", (req, res) => {
+
+    const category = {
+        categoryName: req.body.categoryName
+    };
+
+    Category.create(category)
+        .then((category) => {
+            return Product.updateOne({ _id: req.params.id }, { $push: { categories: category._id } }, { new: true });
+        })
+        .then(products => {
+            // res.render("home", { title: "All The Products with Categories", products: products });
+            res.redirect("/home");
+        })
+        .catch(err => {
+            res.json(err);
+        });  
+});
+
+
+router.get("/home", async (req, res) => {
+    await await Product.find().populate(
+        {
+            path: "categories"
+        }
+    ).exec((err, products) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            res.render("home", {
+                title: "All The Products with Categories",
                 products: products
             });
         }
 
     });
-});
-
-// Get All the Categories
-router.get("/categories", (req, res) => {
-    Category.find().exec((err, categories) => {
-        if (err) {
-            res.json({ message: err.message });
-        } else {
-            res.render("categories", {
-                title: "Categories Page",
-                categories: categories
-            });
-        }
-    });
-});
+})
 
 
-router.get("/addCategory", (req, res) => {
-    res.render("add_category", { title: "Add Category" });
-});
 
 
-router.get("/addProduct", (req, res) => {
-    res.render("add_products", { title: "Add Products" });
-});
+// Insert an Category into database route
+// router.post("/addCategory", (req, res) => {
+//     const category = new Category({
+//         categoryName: req.body.categoryName
+//     });
+
+//     category.save(err => {
+//         if (err) {
+//             res.json({
+//                 message: err.message, type: 'danger'
+//             });
+//         } else {
+//             req.session.message = {
+//                 type: "success",
+//                 message: "Category added successfully!"
+//             }
+            
+//             res.redirect("/categories");
+//         }
+//     });
+// });
+
+
+
+
+
+
+
+
 
 // Edit a Product Route
-router.get("/edit/:id", (req, res) => {
+router.get("/products/:id/edit", (req, res) => {
     let id = req.params.id;
     Product.findById(id, (err, product) => {
         if (err) {
@@ -128,14 +196,14 @@ router.post("/updateProduct/:id", (req, res) => {
                 message: "Product updated successfully!"
             };
 
-            res.redirect("/");
+            res.redirect("/products");
         }
     })
 });
 
 
 // Delete product route
-router.get("/delete/:id", (req, res) => {
+router.get("/products/:id/delete", (req, res) => {
     let id = req.params.id;
 
     Product.deleteOne({ _id: id }, (err, result) => {
@@ -147,7 +215,7 @@ router.get("/delete/:id", (req, res) => {
                 message: "User deleted successfully!"
             }
 
-            res.redirect("/");
+            res.redirect("/products");
         }
     });
 });
