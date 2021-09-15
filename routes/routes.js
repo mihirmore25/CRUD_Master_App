33248b8448f5d6bde3cd1174098610d48aac7de5
Mihir More <mihirmore.25@gmail.com>
2021-09-15 17:53:39 +0530

    Adding product using category id or PK

--- routes/routes.js
+++ routes/routes.js
@@ -5,16 +5,12 @@ const Category = require("../models/category");
 
 // Get All The products route
 router.get("/products", async (req, res) => {
-    await Product.find().populate(
-        {
-            path: "category"
-        }
-    ).exec((err, products) => {
+    await Product.find().exec((err, products) => {
         if (err) {
             res.json({ message: err.message });
         } else {
-            res.render("index", {
-                title: "Home Page",
+            res.render("productsIndex", {
+                title: "Products Page",
                 products: products
             });
         }
@@ -22,15 +18,29 @@ router.get("/products", async (req, res) => {
     });
 });
 
+// Get All The categories route
+router.get("/categories", async (req, res) => {
+    await Category.find().exec((err, categories) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            res.render("categoriesIndex", {
+                title: "Categories Page",
+                categories: categories
+            });
+        }
 
-// Get the add product route
-router.get("/addProduct", (req, res) => {
-    res.render("add_products", { title: "Add Products" });
+    });
 });
 
 
-// Insert an product into database route
-router.post("/addProduct", async (req, res) => {
+// Get the category route
+router.get("/addCategory", (req, res) => {
+    res.render("add_category", { title: "Add Category" })
+});
+
+// Insert an category into database route
+router.post("/addCategory", async (req, res) => {
     const category = new Category({
         categoryName: req.body.categoryName
     });
@@ -38,20 +48,44 @@ router.post("/addProduct", async (req, res) => {
     return category.save()
         .then(category => {
             console.log(category);
-
-            const categoryId = category._id;
-
-            const product = new Product({
-                productName: req.body.productName,
-                category: categoryId
-            });
-
-            product.save();
-
-            // return product;
-
-            res.redirect("/products");
+            res.redirect("/categories");
         });
+});
+
+
+// Get the add product route
+router.get("/categories/:id/addProduct", (req, res) => {
+    let id = req.params.id;
+
+    Category.findById(id).exec((err, category) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            res.render("add_products", { title: "Add Product", category: category })
+        }
+    });
+});
+
+
+// Insert an product into database route
+router.post("/categories/:id/addProduct", async (req, res) => {
+    let id = req.params.id;
+
+    await Category.findById(id).exec((err, category) => {
+        if (err) {
+            res.json({ message: err.message });
+        }
+
+        console.log(category);
+        const product = new Product({
+            productName: req.body.productName,
+            category: category._id
+        });
+
+        product.save();
+
+        res.redirect("/home");
+    });
 });
 
 
@@ -99,7 +133,7 @@ router.get("/products/:id/edit", (req, res) => {
     });
 });
 
-// Update product route
+// Update a product route
 router.post("/updateProduct/:id", (req, res) => {
     let id = req.params.id;
 
@@ -122,6 +156,9 @@ router.post("/updateProduct/:id", (req, res) => {
 });
 
 
+
+
+
 // Delete product route
 router.get("/products/:id/delete", (req, res) => {
     let id = req.params.id;
@@ -132,7 +169,7 @@ router.get("/products/:id/delete", (req, res) => {
         } else {
             req.session.message = {
                 type: "info",
-                message: "User deleted successfully!"
+                message: "Product deleted successfully!"
             }
 
             res.redirect("/products");
@@ -141,8 +178,76 @@ router.get("/products/:id/delete", (req, res) => {
 });
 
 
-// Adding pagination to the products route
-router.get('/products/:page', function (req, res, next) {
+// Edit a Category Route
+router.get("/categories/:id/edit", (req, res) => {
+    let id = req.params.id;
+    Category.findById(id, (err, category) => {
+        if (err) {
+            res.redirect("/");
+        } else {
+            if (category == null) {
+                res.redirect("/");
+            } else {
+                res.render("edit_category", {
+                    title: "Edit Category",
+                    category: category
+                });
+            }
+
+        }
+
+    });
+});
+
+// Update a Category route
+router.post("/updateCategory/:id", (req, res) => {
+    let id = req.params.id;
+
+    Category.updateOne({ _id: id }, {
+        categoryName: req.body.categoryName
+    }, (err, result) => {
+        if (err) {
+            res.json({
+                message: err.message, type: 'danger'
+            });
+        } else {
+            req.session.message = {
+                type: "success",
+                message: "Category updated successfully!"
+            };
+
+            res.redirect("/categories");
+        }
+    })
+});
+
+
+
+
+
+// Delete category route
+router.get("/categories/:id/delete", (req, res) => {
+    let id = req.params.id;
+
+    Category.deleteOne({ _id: id }, (err, result) => {
+        if (err) {
+            res.json({ message: err.message });
+        } else {
+            req.session.message = {
+                type: "info",
+                message: "Category deleted successfully!"
+            }
+
+            res.redirect("/categories");
+        }
+    });
+});
+
+
+
+// Adding pagination to the home route 
+// where all the products and categories will be
+router.get('/home/:page', function (req, res, next) {
     var perPage = 9
     var page = req.params.page || 1
 
@@ -155,8 +260,49 @@ router.get('/products/:page', function (req, res, next) {
             Product.countDocuments().exec(function (err, count) {
                 if (err) return next(err)
                 res.render("home", {
+                    title: "All The Products with Pagination On Home Page",
+                    products: products,
+                });
+            });
+        });
+});
+
+// Adding pagination to the products route
+router.get('/products/:page', function (req, res, next) {
+    var perPage = 9
+    var page = req.params.page || 1
+
+    Product
+        .find()
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function (err, products) {
+            Product.countDocuments().exec(function (err, count) {
+                if (err) return next(err)
+                res.render("productsIndex", {
                     title: "All The Products with Pagination",
                     products: products,
+                });
+            });
+        });
+});
+
+
+// Adding pagination to the categories route
+router.get('/categories/:page', function (req, res, next) {
+    var perPage = 9
+    var page = req.params.page || 1
+
+    Category
+        .find()
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function (err, categories) {
+            Category.countDocuments().exec(function (err, count) {
+                if (err) return next(err)
+                res.render("categoriesIndex", {
+                    title: "All The Categories with Pagination",
+                    categories: categories,
                 });
             });
         });
